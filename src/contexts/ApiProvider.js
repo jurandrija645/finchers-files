ec79843ca43849c457b170506e691edb0ea1b063
Andrija Juran <Andrija.Juran@in2.eu>
2025-01-16 14:09:56 +0100

    APi provider addition

--- src/contexts/ApiProvider.js
+++ src/contexts/ApiProvider.js
@@ -1,21 +1,19 @@
-import { createContext, useContext } from 'react';
-import FinchersApiClient from '../FinchersApiClient';
+import { createContext, useContext } from "react";
+import FinchersApiClient from "../FinchersApiClient";
 
-const ApiContext = createContext();
+const ApiContext = createContext(); // creating context
 
-export default function ApiProvider({children})
-{
-    const api = new FinchersApiClient();
+export default function ApiProvider({ children }) {
+  const api = new FinchersApiClient();
 
-    return (
-        <ApiContext.Provider value={api}>
-            {children}
-        </ApiContext.Provider>
-    );
+  return (
+    <ApiContext.Provider value={api}>{children}</ApiContext.Provider>
+    /**The component returns the apicontext that was created before but the provider part of it
+     * The value of the context is an object of the apiClient
+     */
+  );
 }
 
-export function useApi()
-{
-    return useContext(ApiContext);
+export function useApi() {
+  return useContext(ApiContext);
 }
-
